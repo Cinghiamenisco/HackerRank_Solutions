1d07fe7e2094d51f0281ffccd0f0d02d47b9714b
Cinghiamenisco <42942098+Cinghiamenisco@users.noreply.github.com>
2018-09-04 10:35:21 +0200

    Create 09 - [Easy] - Birthday Cake Candles.java

---
+++ Problem Solving/WarmUp/09 - [Easy] - Birthday Cake Candles.java
@@ -0,0 +1,61 @@
+// Autore:      Christian Luzzetti
+// Mail:        Christian.luzzetti@gmail.com
+// HackerRank:  https://www.hackerrank.com/Cinghiamenisco
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    // Complete the birthdayCakeCandles function below.
+    static int birthdayCakeCandles(int[] ar) {
+
+        Arrays.sort(ar);
+        
+        int highest_candle = ar[ar.length-1];
+        int total_candle_blown = 0;
+        
+        for (int i=ar.length-1; i>=0; i--) {
+            if (ar[i] == highest_candle) {
+                total_candle_blown++;
+            }
+        }
+        
+        return total_candle_blown;
+
+
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int arCount = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        int[] ar = new int[arCount];
+
+        String[] arItems = scanner.nextLine().split(" ");
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int i = 0; i < arCount; i++) {
+            int arItem = Integer.parseInt(arItems[i]);
+            ar[i] = arItem;
+        }
+
+        int result = birthdayCakeCandles(ar);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
+}
