8d1c9f8f212137a34a553670eb31926335673bf3
Cinghiamenisco <42942098+Cinghiamenisco@users.noreply.github.com>
2018-09-04 09:34:04 +0200

    Create 20 - [Easy] - Employee Salaries.sql

---
+++ SQL/Basic Select/20 - [Easy] - Employee Salaries.sql
@@ -0,0 +1,9 @@
+-- Autore:      Christian Luzzetti
+-- Mail:        Christian.luzzetti@gmail.com
+-- HackerRank:  https://www.hackerrank.com/Cinghiamenisco
+
+select  e.name
+from employee e
+where e.salary > 2000
+and e.months < 10
+;
