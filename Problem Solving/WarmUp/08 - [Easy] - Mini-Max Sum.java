affc8dbdf6e24d006d40cdfbebbcd8858934e33c
Cinghiamenisco <42942098+Cinghiamenisco@users.noreply.github.com>
2018-09-04 10:34:39 +0200

    Create 08 - [Easy] - Mini-Max Sum.java

---
+++ Problem Solving/WarmUp/08 - [Easy] - Mini-Max Sum.java
@@ -0,0 +1,51 @@
+// Autore:      Christian Luzzetti
+// Mail:        Christian.luzzetti@gmail.com
+// HackerRank:  https://www.hackerrank.com/Cinghiamenisco
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+
+public class Solution {
+
+    // Complete the miniMaxSum function below.
+    static void miniMaxSum(int[] arr) {
+
+        long minSum = 0;
+        long maxSum = 0;
+        Arrays.sort(arr);
+        for (int i=0; i<4; i++) {
+            minSum = minSum + arr[i];
+        }
+        for(int i=arr.length; i>arr.length-4; i--) {
+            maxSum = maxSum + arr[i-1];
+        }
+        
+        System.out.printf("%d %d",minSum,maxSum);
+    }
+    
+    //Codice Preesistente
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int[] arr = new int[5];
+
+        String[] arrItems = scanner.nextLine().split(" ");
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int i = 0; i < 5; i++) {
+            int arrItem = Integer.parseInt(arrItems[i]);
+            arr[i] = arrItem;
+        }
+
+        miniMaxSum(arr);
+
+        scanner.close();
+    }
+}
