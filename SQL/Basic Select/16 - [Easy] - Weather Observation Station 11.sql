fe459e2d24aa1925ccb9949936cf4a43caf72c8b
Cinghiamenisco <42942098+Cinghiamenisco@users.noreply.github.com>
2018-09-04 09:31:56 +0200

    Create 16 - [Easy] - Weather Observation Station 11.sql

---
+++ SQL/Basic Select/16 - [Easy] - Weather Observation Station 11.sql
@@ -0,0 +1,23 @@
+-- Autore:      Christian Luzzetti
+-- Mail:        Christian.luzzetti@gmail.com
+-- HackerRank:  https://www.hackerrank.com/Cinghiamenisco
+
+select distinct(s.city)
+from station s
+where 1=1
+and (
+    UPPER(s.city) not like 'A%'
+and UPPER(s.city) not like 'E%'
+and UPPER(s.city) not like 'I%'
+and UPPER(s.city) not like 'O%'
+and UPPER(s.city) not like 'U%'
+     )
+OR
+(
+UPPER(s.city) not like '%A'
+and UPPER(s.city) not like '%E'
+and UPPER(s.city) not like '%I'
+and UPPER(s.city) not like '%O'
+and UPPER(s.city) not like '%U'
+)
+;
