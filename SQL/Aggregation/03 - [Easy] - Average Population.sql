7ba8f960c8655545f2644966a7405855377acdff
Cinghiamenisco <42942098+Cinghiamenisco@users.noreply.github.com>
2018-09-04 10:06:56 +0200

    Create 03 - [Easy] - Average Population.sql

---
+++ SQL/Aggregation/03 - [Easy] - Average Population.sql
@@ -0,0 +1,7 @@
+-- Autore:      Christian Luzzetti
+-- Mail:        Christian.luzzetti@gmail.com
+-- HackerRank:  https://www.hackerrank.com/Cinghiamenisco
+
+SELECT floor(avg(c.population))
+FROM    city c
+;
