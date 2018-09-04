d888f6b81f7532d6eb16a3d61737ce62788989c3
Cinghiamenisco <42942098+Cinghiamenisco@users.noreply.github.com>
2018-09-04 10:16:27 +0200

    Create 05 - [Easy] - Diagonal Difference.java

---
+++ Problem Solving/05 - [Easy] - Diagonal Difference.java
@@ -0,0 +1,68 @@
+-- Autore:      Christian Luzzetti
+-- Mail:        Christian.luzzetti@gmail.com
+-- HackerRank:  https://www.hackerrank.com/Cinghiamenisco
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    // Complete the diagonalDifference function below.
+    static int diagonalDifference(int[][] arr) {
+    
+        int leftTOright = 0;
+        int rightTOleft = 0;
+        for (int i=0; i<arr.length; i++) {
+            for (int k=0; k<arr.length; k++) {
+                
+                if (k == i) {
+                    leftTOright += arr[i][k];
+                } 
+                
+                if (k == arr.length - 1 - i) {
+                    rightTOleft += arr[i][k];                    
+                }
+                
+            }
+        }
+
+        return Math.abs(leftTOright - rightTOleft);
+    }
+    
+// Codice Preesistente  
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int n = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        int[][] arr = new int[n][n];
+
+        for (int i = 0; i < n; i++) {
+            String[] arrRowItems = scanner.nextLine().split(" ");
+            scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+            for (int j = 0; j < n; j++) {
+                int arrItem = Integer.parseInt(arrRowItems[j]);
+                arr[i][j] = arrItem;
+            }
+        }
+
+        int result = diagonalDifference(arr);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
+}
