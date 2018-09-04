36a42dff0eadadf62adad5530f1d8c3e453d7acf
Cinghiamenisco <42942098+Cinghiamenisco@users.noreply.github.com>
2018-09-04 09:33:40 +0200

    Create 19 - [Easy] - Employee Names.sql

---
+++ SQL/Basic Select/19 - [Easy] - Employee Names.sql
@@ -0,0 +1,8 @@
+-- Autore:      Christian Luzzetti
+-- Mail:        Christian.luzzetti@gmail.com
+-- HackerRank:  https://www.hackerrank.com/Cinghiamenisco
+
+select e.name
+from employee e
+order by e.name asc
+;
