ef01c3f0b4110a74d6107a896eb50631a3c8b342
Christian <42942098+Cinghiamenisco@users.noreply.github.com>
2018-09-06 12:07:04 +0200

    Create 05 - [Easy] - Population Density Difference.sql

---
+++ SQL/Aggregation/05 - [Easy] - Population Density Difference.sql
@@ -0,0 +1,7 @@
+-- Autore:      Christian Luzzetti
+-- Mail:        Christian.luzzetti@gmail.com
+-- HackerRank:  https://www.hackerrank.com/Cinghiamenisco
+
+select max(population) - min(population)
+from city
+;
