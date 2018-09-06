7a06b1b8595175693d0eb816cc806ff78565c61e
Christian <42942098+Cinghiamenisco@users.noreply.github.com>
2018-09-06 12:05:06 +0200

    Create 04 - [Easy] - Japan Population.sql

---
+++ SQL/Aggregation/04 - [Easy] - Japan Population.sql
@@ -0,0 +1,9 @@
+-- Autore:      Christian Luzzetti
+-- Mail:        Christian.luzzetti@gmail.com
+-- HackerRank:  https://www.hackerrank.com/Cinghiamenisco
+
+select sum(c.population)
+from CITY c
+where 1=1
+and c.countrycode = 'JPN'
+;
