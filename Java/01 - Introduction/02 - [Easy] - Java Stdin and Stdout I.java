cdae08c861dc4978a000de808bd27d3e526ba5d5
Christian <42942098+Cinghiamenisco@users.noreply.github.com>
2018-09-07 17:35:25 +0200

    Create 02 - [Easy] - Java Stdin and Stdout I.java

---
+++ Java/01 - Introduction/02 - [Easy] - Java Stdin and Stdout I.java
@@ -0,0 +1,19 @@
+// Autore:      Christian Luzzetti
+// Mail:        Christian.luzzetti@gmail.com
+// HackerRank:  https://www.hackerrank.com/Cinghiamenisco
+
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int a = scan.nextInt();
+        int b = scan.nextInt();
+        int c = scan.nextInt();
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+    }
+}
