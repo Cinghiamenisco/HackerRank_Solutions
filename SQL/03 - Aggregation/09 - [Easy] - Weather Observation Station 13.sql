b1ff04f753e4cf6142ce06eaa0078564889835a9
Christian <42942098+Cinghiamenisco@users.noreply.github.com>
2018-09-07 17:23:55 +0200

    Create 09 - [Easy] - Weather Observation Station 13.sql

---
+++ SQL/03 - Aggregation/09 - [Easy] - Weather Observation Station 13.sql
@@ -0,0 +1,9 @@
+-- Autore:      Christian Luzzetti
+-- Mail:        Christian.luzzetti@gmail.com
+-- HackerRank:  https://www.hackerrank.com/Cinghiamenisco
+
+select TRUNC(sum(LAT_N),4)
+from station
+where lat_n > 38.7880
+and lat_n < 137.2345
+;
