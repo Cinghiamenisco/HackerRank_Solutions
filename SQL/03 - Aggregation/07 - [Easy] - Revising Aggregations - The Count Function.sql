6955e7ef5fcab6a74eb7975db565ea8a177a8219
Christian <42942098+Cinghiamenisco@users.noreply.github.com>
2018-09-07 17:19:01 +0200

    Create 07 - [Easy] - Revising Aggregations - The Count Function.sql

---
+++ SQL/03 - Aggregation/07 - [Easy] - Revising Aggregations - The Count Function.sql
@@ -0,0 +1,8 @@
+-- Autore:      Christian Luzzetti
+-- Mail:        Christian.luzzetti@gmail.com
+-- HackerRank:  https://www.hackerrank.com/Cinghiamenisco
+
+select count(id)
+from city
+where population > 100000
+;
