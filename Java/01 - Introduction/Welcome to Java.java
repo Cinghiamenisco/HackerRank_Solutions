456227981436e12edbf98c82c853645b54690b7c
Christian <42942098+Cinghiamenisco@users.noreply.github.com>
2018-09-07 17:32:43 +0200

    Create Welcome to Java.java

---
+++ Java/01 - Introduction/Welcome to Java.java
@@ -0,0 +1,12 @@
+// Autore:      Christian Luzzetti
+// Mail:        Christian.luzzetti@gmail.com
+// HackerRank:  https://www.hackerrank.com/Cinghiamenisco
+
+public class Solution {
+    public static void main(String[] args) {
+    
+        System.out.println("Hello, World.");
+        System.out.println("Hello, Java.");
+        
+    }
+}
