7039eb08c25c916467ae8690162416a38202642f
Christian <42942098+Cinghiamenisco@users.noreply.github.com>
2018-09-07 17:21:28 +0200

    Create 08 - [Easy] - Weather Observation Station 2.sql

---
+++ SQL/03 - Aggregation/08 - [Easy] - Weather Observation Station 2.sql
@@ -0,0 +1,7 @@
+-- Autore:      Christian Luzzetti
+-- Mail:        Christian.luzzetti@gmail.com
+-- HackerRank:  https://www.hackerrank.com/Cinghiamenisco
+
+select round(sum(LAT_N),2), round(sum(LONG_W),2)
+from station
+;
