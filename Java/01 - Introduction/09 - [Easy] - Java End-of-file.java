b34d7219407c77cc81845ffff58279368dad4ddb
Christian <42942098+Cinghiamenisco@users.noreply.github.com>
2018-09-10 09:35:45 +0200

    Create 09 - [Easy] - Java End-of-file.java

---
+++ Java/01 - Introduction/09 - [Easy] - Java End-of-file.java
@@ -0,0 +1,24 @@
+// Autore:      Christian Luzzetti
+// Mail:        Christian.luzzetti@gmail.com
+// HackerRank:  https://www.hackerrank.com/Cinghiamenisco
+
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner scanner = new Scanner(System.in);
+        String s;
+        int line = 0;
+        
+        while (scanner.hasNextLine()) {            
+            System.out.println((++line) + " " + scanner.nextLine());
+        }
+        
+    }
+}
