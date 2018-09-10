7f2d2b3388e51950d7913877ff8d582111a3c19e
Christian <42942098+Cinghiamenisco@users.noreply.github.com>
2018-09-10 09:57:21 +0200

    Create 10 - [Easy] - Java Static Initializer Block.java

---
+++ Java/01 - Introduction/10 - [Easy] - Java Static Initializer Block.java
@@ -0,0 +1,26 @@
+// Autore:      Christian Luzzetti
+// Mail:        Christian.luzzetti@gmail.com
+// HackerRank:  https://www.hackerrank.com/Cinghiamenisco
+
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+static Scanner scan = new Scanner(System.in);
+static int B = scan.nextInt();
+static int H = scan.nextInt();
+static boolean flag = true;
+
+public static void main(String[] args){
+		if(flag){
+			int area=B*H;
+			System.out.print(area);
+		}
+		
+	}//end of main
+
+}//end of class
+
