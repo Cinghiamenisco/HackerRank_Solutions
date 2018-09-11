357287caa64a54f573fe7045e7bd635c01325cea
Christian <42942098+Cinghiamenisco@users.noreply.github.com>
2018-09-11 13:37:09 +0200

    Create [Easy] - Python Division.py

---
+++ Python/01 - Introduction/[Easy] - Python Division.py
@@ -0,0 +1,9 @@
+# Autore:      Christian Luzzetti
+# Mail:        Christian.luzzetti@gmail.com
+# HackerRank:  https://www.hackerrank.com/Cinghiamenisco
+
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+    print(a//b)
+    print(a/b)
