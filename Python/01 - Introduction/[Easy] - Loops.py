1ca4f3d6838964cf4ed08561051892b8366d3a6b
Christian <42942098+Cinghiamenisco@users.noreply.github.com>
2018-09-11 13:40:01 +0200

    Create [Easy] - Loops.py

---
+++ Python/01 - Introduction/[Easy] - Loops.py
@@ -0,0 +1,8 @@
+# Autore:      Christian Luzzetti
+# Mail:        Christian.luzzetti@gmail.com
+# HackerRank:  https://www.hackerrank.com/Cinghiamenisco
+
+if __name__ == '__main__':
+    n = int(input())
+    for x in range(0,n):
+        print(x**2)
