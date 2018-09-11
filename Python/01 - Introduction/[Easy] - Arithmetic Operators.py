aa3c1d2d6fa5c74cd882927184dd7a7b258fae14
Christian <42942098+Cinghiamenisco@users.noreply.github.com>
2018-09-11 13:36:05 +0200

    Create [Easy] - Arithmetic Operators.py

---
+++ Python/01 - Introduction/[Easy] - Arithmetic Operators.py
@@ -0,0 +1,10 @@
+# Autore:      Christian Luzzetti
+# Mail:        Christian.luzzetti@gmail.com
+# HackerRank:  https://www.hackerrank.com/Cinghiamenisco
+
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+    print(a+b)
+    print(a-b)
+    print(a*b)
