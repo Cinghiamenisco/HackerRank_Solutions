266fd7067c4c9e2748f05643b26c9f6ee570cd57
Christian <42942098+Cinghiamenisco@users.noreply.github.com>
2018-09-12 10:35:22 +0200

    Create [Easy] - Java 1D Array.java

---
+++ JavaData Structure/[Easy] - Java 1D Array.java
@@ -0,0 +1,28 @@
+// Autore:      Christian Luzzetti
+// Mail:        Christian.luzzetti@gmail.com
+// HackerRank:  https://www.hackerrank.com/Cinghiamenisco
+
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+	   
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+        
+        
+        int[] a = new int[n];
+
+        for(int i=0; i<n; i++) {
+            a[i] = scan.nextInt();            
+        }
+        
+        scan.close();
+
+        // Prints each sequential element in array a
+        for (int i = 0; i < a.length; i++) {
+            System.out.println(a[i]);
+        }
+    }
+}
