bd9254a9e3a3c98a9912c19ad6ee7eba702383d9
Christian <42942098+Cinghiamenisco@users.noreply.github.com>
2018-09-13 15:06:39 +0200

    Create [Easy] - Delete a Node.java

---
+++ Problem Solving/Data Structure/LinkedList/[Easy] - Delete a Node.java
@@ -0,0 +1,23 @@
+// Autore:      Christian Luzzetti
+// Mail:        Christian.luzzetti@gmail.com
+// HackerRank:  https://www.hackerrank.com/Cinghiamenisco
+
+    // Complete the deleteNode function below.
+
+    /*
+     * For your reference:
+     *
+     * SinglyLinkedListNode {
+     *     int data;
+     *     SinglyLinkedListNode next;
+     * }
+     *
+     */
+    static SinglyLinkedListNode deleteNode(SinglyLinkedListNode head, int position) {
+
+        if (position == 0) {
+            return head.next;
+        }
+        head.next = deleteNode(head.next, --position);
+        return head;
+    }
