bf40a38c5ee53d133bc7b532a9b9fb1a3e13c170
Christian <42942098+Cinghiamenisco@users.noreply.github.com>
2018-09-18 14:28:01 +0200

    Create Day 0 - Hello World.py

---
+++ 30 Days Of Code/Day 0 - Hello World.py
@@ -0,0 +1,9 @@
+# Autore:      Christian Luzzetti
+# Mail:        Christian.luzzetti@gmail.com
+# HackerRank:  https://www.hackerrank.com/Cinghiamenisco
+
+input_string = input()
+
+print('Hello, World.')
+
+print(input_string)
