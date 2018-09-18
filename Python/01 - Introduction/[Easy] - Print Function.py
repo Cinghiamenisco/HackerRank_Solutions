6aff8149b563e7abf8dd3c824aa33f561394b1bb
Christian <42942098+Cinghiamenisco@users.noreply.github.com>
2018-09-18 14:04:07 +0200

    Create [Easy] - Print Function.py

---
+++ Python/01 - Introduction/[Easy] - Print Function.py
@@ -0,0 +1,10 @@
+# Autore:      Christian Luzzetti
+# Mail:        Christian.luzzetti@gmail.com
+# HackerRank:  https://www.hackerrank.com/Cinghiamenisco
+
+from __future__ import print_function
+
+if __name__ == '__main__':
+    n = int(raw_input())
+    for i in range(0,n):
+        print(i+1, end='');
