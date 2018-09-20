c82208d8b797fb118a40e466635569b16ff63ac9
Christian <42942098+Cinghiamenisco@users.noreply.github.com>
2018-09-20 15:36:25 +0200

    Update 08 - [Medium] - Java Regex.java

--- Java/02 - Strings/08 - [Medium] - Java Regex.java
+++ Java/02 - Strings/08 - [Medium] - Java Regex.java
@@ -2,3 +2,9 @@
 // Mail:        Christian.luzzetti@gmail.com
 // HackerRank:  https://www.hackerrank.com/Cinghiamenisco
 
+//Write your code here
+class MyRegex {
+    
+    String pattern = "^(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$";
+            
+}
