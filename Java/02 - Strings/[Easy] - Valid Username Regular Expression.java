529d34b2135edc75c1306eab505213267e21b94c
Christian <42942098+Cinghiamenisco@users.noreply.github.com>
2018-09-24 12:14:16 +0200

    Create [Easy] - Valid Username Regular Expression.java

---
+++ Java/02 - Strings/[Easy] - Valid Username Regular Expression.java
@@ -0,0 +1,7 @@
+// Autore:      Christian Luzzetti
+// Mail:        Christian.luzzetti@gmail.com
+// HackerRank:  https://www.hackerrank.com/Cinghiamenisco
+
+class UsernameValidator {
+    public static final String regularExpression = "^[a-zA-Z][a-zA-Z0-9_]{7,29}$";
+}
