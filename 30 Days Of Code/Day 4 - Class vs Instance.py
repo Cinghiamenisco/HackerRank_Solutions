56ba1fdb2b223ba3d2bdcb924b5ef854cbe7cbd4
Christian <42942098+Cinghiamenisco@users.noreply.github.com>
2018-09-24 09:31:34 +0200

    Create Day 4 - Class vs Instance.py

---
+++ 30 Days Of Code/Day 4 - Class vs Instance.py
@@ -0,0 +1,24 @@
+# Autore:      Christian Luzzetti
+# Mail:        Christian.luzzetti@gmail.com
+# HackerRank:  https://www.hackerrank.com/Cinghiamenisco
+
+class Person:
+    age = 0
+    
+    def __init__(self,initialAge):
+        if initialAge >= 0:
+            self.age = initialAge
+        else:
+            self.age = 0
+            print("Age is not valid, setting age to 0.")
+            
+    def amIOld(self):
+        if self.age < 13:
+            print("You are young.")
+        elif self.age >= 13 and self.age<18:
+            print("You are a teenager.")
+        else:
+            print("You are old.")
+        
+    def yearPasses(self):
+        self.age+=1
