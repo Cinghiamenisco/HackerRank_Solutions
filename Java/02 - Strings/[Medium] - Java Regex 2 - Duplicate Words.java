911991379de44d312b9e02edd9c3d8b63a195888
Christian <42942098+Cinghiamenisco@users.noreply.github.com>
2018-09-24 15:22:38 +0200

    Update [Medium] - Java Regex 2 - Duplicate Words.java

--- Java/02 - Strings/[Medium] - Java Regex 2 - Duplicate Words.java
+++ Java/02 - Strings/[Medium] - Java Regex 2 - Duplicate Words.java
@@ -2,3 +2,36 @@
 // Mail:        Christian.luzzetti@gmail.com
 // HackerRank:  https://www.hackerrank.com/Cinghiamenisco
 
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class DuplicateWords {
+
+    public static void main(String[] args) {
+
+        String regex = "\\b(\\w+)(\\s\\1)+\\b";
+        
+        //Flag regex!
+        Pattern p = Pattern.compile(regex, Pattern.CASE_INSENSITIVE);
+
+        Scanner in = new Scanner(System.in);
+        int numSentences = Integer.parseInt(in.nextLine());
+        
+        while (numSentences-- > 0) {
+            String input = in.nextLine();
+            
+            Matcher m = p.matcher(input);
+            
+            while (m.find()) {
+                //LINEA IMPORTANTE!!! Sostituzione tra gruppi di regex. Il vuoto è tutta la stringa matchata.
+                input = input.replaceAll(m.group(), m.group(1));
+            }
+            
+            // Prints the modified sentence.
+            System.out.println(input);
+        }
+        
+        in.close();
+    }
+}
