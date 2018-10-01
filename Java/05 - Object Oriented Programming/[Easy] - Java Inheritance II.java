04c5ef3295f5a89705b287932b596846f0dfc363
Christian <42942098+Cinghiamenisco@users.noreply.github.com>
2018-10-01 16:31:30 +0200

    Create [Easy] - Java Inheritance II.java

---
+++ Java/05 - Object Oriented Programming/[Easy] - Java Inheritance II.java
@@ -0,0 +1,12 @@
+// Autore:      Christian Luzzetti
+// Mail:        Christian.luzzetti@gmail.com
+// HackerRank:  https://www.hackerrank.com/Cinghiamenisco
+
+//Write your code here
+class Arithmetic {
+    public int add(int a, int b) { return a+b; }
+}
+
+class Adder extends Arithmetic {
+    
+}
