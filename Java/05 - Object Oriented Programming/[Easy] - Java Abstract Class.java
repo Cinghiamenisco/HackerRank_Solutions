5d6878c160be546d1b26956d94bf844f8e990731
Christian <42942098+Cinghiamenisco@users.noreply.github.com>
2018-10-01 16:50:08 +0200

    Create [Easy] - Java Abstract Class.java

---
+++ Java/05 - Object Oriented Programming/[Easy] - Java Abstract Class.java
@@ -0,0 +1,16 @@
+// Autore:      Christian Luzzetti
+// Mail:        Christian.luzzetti@gmail.com
+// HackerRank:  https://www.hackerrank.com/Cinghiamenisco
+
+//Write MyBook class here
+class MyBook extends Book {
+    
+    public MyBook() {
+        super();
+    }
+    
+    public void setTitle (String s) { 
+        this.title = s; 
+    }
+        
+}
