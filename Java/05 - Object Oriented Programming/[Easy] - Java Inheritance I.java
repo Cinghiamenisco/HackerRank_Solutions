c384ee8c5d05add6019027a029c201c756c8051a
Christian <42942098+Cinghiamenisco@users.noreply.github.com>
2018-10-01 16:28:36 +0200

    Create [Easy] - Java Inheritance I.java

---
+++ Java/05 - Object Oriented Programming/[Easy] - Java Inheritance I.java
@@ -0,0 +1,15 @@
+// Autore:      Christian Luzzetti
+// Mail:        Christian.luzzetti@gmail.com
+// HackerRank:  https://www.hackerrank.com/Cinghiamenisco
+
+class Bird extends Animal
+{
+	void fly()
+	{
+		System.out.println("I am flying");
+	}
+    
+    void sing() {
+    System.out.println("I am singing");
+    }
+}
