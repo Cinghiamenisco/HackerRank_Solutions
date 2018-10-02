d9d190d80d37f3d096f2775a665f427d684cc06a
Christian <42942098+Cinghiamenisco@users.noreply.github.com>
2018-10-02 12:07:17 +0200

    Create [Easy] - Java Method Overriding.java

---
+++ Java/05 - Object Oriented Programming/[Easy] - Java Method Overriding.java
@@ -0,0 +1,9 @@
+// Autore:      Christian Luzzetti
+// Mail:        Christian.luzzetti@gmail.com
+// HackerRank:  https://www.hackerrank.com/Cinghiamenisco
+
+
+    @Override
+    void getNumberOfTeamMembers() {
+        System.out.println("Each team has 11 players in " + getName() );
+    }
