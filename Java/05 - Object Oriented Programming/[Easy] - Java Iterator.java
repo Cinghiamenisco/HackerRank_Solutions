2eabdb0446b5c0c535a6ab8f591d286c1b9c66b0
Christian <42942098+Cinghiamenisco@users.noreply.github.com>
2018-10-02 13:44:25 +0200

    Create [Easy] - Java Iterator.java

---
+++ Java/05 - Object Oriented Programming/[Easy] - Java Iterator.java
@@ -0,0 +1,6 @@
+// Autore:      Christian Luzzetti
+// Mail:        Christian.luzzetti@gmail.com
+// HackerRank:  https://www.hackerrank.com/Cinghiamenisco
+
+         Object element = it.next();
+         if(!(element instanceof Integer))//Hints: use instanceof operator
