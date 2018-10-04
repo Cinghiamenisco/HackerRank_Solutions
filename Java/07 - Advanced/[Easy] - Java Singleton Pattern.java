531c1b900722fff73bd3dc6f2969f7fd4ed1d56b
Christian <42942098+Cinghiamenisco@users.noreply.github.com>
2018-10-04 11:09:26 +0200

    Create [Easy] - Java Singleton Pattern.java

---
+++ Java/07 - Advanced/[Easy] - Java Singleton Pattern.java
@@ -0,0 +1,29 @@
+// Autore:      Christian Luzzetti
+// Mail:        Christian.luzzetti@gmail.com
+// HackerRank:  https://www.hackerrank.com/Cinghiamenisco
+
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+import java.lang.reflect.*;
+
+
+class Singleton {
+    
+    private static Singleton instance;
+    public String str = "hello world";
+   
+    private Singleton() {}
+    
+    public static Singleton getSingleInstance()
+    {
+    if (instance == null)
+    {
+      instance = new Singleton();
+    }
+    return instance; 
+  }
+
+}
