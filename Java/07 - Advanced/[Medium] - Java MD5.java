2956c8436b65d71737727f0e6bc577a9c31d5b38
Christian <42942098+Cinghiamenisco@users.noreply.github.com>
2018-10-05 13:58:17 +0200

    Create [Medium] - Java MD5.java

---
+++ Java/07 - Advanced/[Medium] - Java MD5.java
@@ -0,0 +1,36 @@
+// Autore:      Christian Luzzetti
+// Mail:        Christian.luzzetti@gmail.com
+// HackerRank:  https://www.hackerrank.com/Cinghiamenisco
+
+import java.io.*;
+import java.util.*;
+import java.security.*;
+import java.lang.Exception;
+
+public class Solution {
+
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+        String s = scan.nextLine();
+        
+        StringBuffer sb = new StringBuffer();
+
+        
+         try {
+            MessageDigest md = MessageDigest.getInstance("MD5");  
+            md.update(s.getBytes());
+            byte[] digest = md.digest();
+
+            for (byte b : digest) {
+                sb.append(String.format("%02x", b & 0xff));
+            }
+             
+         } catch (Exception e) {
+             System.out.println(e);
+         }
+        
+        System.out.println(sb.toString());
+        
+    }
+}
