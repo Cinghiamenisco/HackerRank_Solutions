50544f649a2101ac70d98b6d3abb774ea79aef73
Cinghiamenisco <christian.luzzetti@gmail.com>
2018-10-07 21:32:11 +0200

    Python exercise

---
+++ 30 Days Of Code/Day 7 - Arrays.py
@@ -0,0 +1,22 @@
+#!/bin/python3
+
+# Autore:      Christian Luzzetti
+# Mail:        Christian.luzzetti@gmail.com
+# HackerRank:  https://www.hackerrank.com/Cinghiamenisco
+
+
+import math
+import os
+import random
+import re
+import sys
+
+
+
+if __name__ == '__main__':
+    n = int(input())
+
+    arr = list(map(int, input().rstrip().split()))
+
+    for i in range (len(arr)-1, -1, -1):
+        print(arr[i], end=' ')
