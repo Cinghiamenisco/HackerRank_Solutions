a2190003b37d049357367209da7cea75f2248fa0
Christian <42942098+Cinghiamenisco@users.noreply.github.com>
2018-10-09 13:55:32 +0200

    Create Day 16 - Exceptions - String to Integer.py

---
+++ 30 Days Of Code/Day 16 - Exceptions - String to Integer.py
@@ -0,0 +1,15 @@
+#!/bin/python3
+
+# Autore:      Christian Luzzetti
+# Mail:        Christian.luzzetti@gmail.com
+# HackerRank:  https://www.hackerrank.com/Cinghiamenisco
+
+import sys
+
+S = input().strip()
+
+try:
+    print(int(S))
+except:
+    print("Bad String")
+            
