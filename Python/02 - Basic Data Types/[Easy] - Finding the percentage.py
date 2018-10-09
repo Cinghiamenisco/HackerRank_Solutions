bf796b4d014756cbf7cab6f6014d53a58753e352
Christian <42942098+Cinghiamenisco@users.noreply.github.com>
2018-10-09 17:44:54 +0200

    Create [Easy] - Finding the percentage.py

---
+++ Python/02 - Basic Data Types/[Easy] - Finding the percentage.py
@@ -0,0 +1,21 @@
+# Autore:      Christian Luzzetti
+# Mail:        Christian.luzzetti@gmail.com
+# HackerRank:  https://www.hackerrank.com/Cinghiamenisco
+
+if __name__ == '__main__':
+    n = int(input())
+    student_marks = {}
+    for _ in range(n):
+        name, *line = input().split()
+        scores = list(map(float, line))
+        student_marks[name] = scores
+        
+    query_name = input()
+    tot = 0.00
+    count = 0
+    for m in student_marks[query_name]:
+        tot+=m
+        count+=1
+        
+    print("%.2f" % (tot/count))
+        
