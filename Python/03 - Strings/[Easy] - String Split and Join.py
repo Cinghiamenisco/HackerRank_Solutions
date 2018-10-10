417bb014a1e0e6709b209c4d5ab6bcab10c78ae2
Christian <42942098+Cinghiamenisco@users.noreply.github.com>
2018-10-10 09:38:40 +0200

    Create [Easy] - String Split and Join.py

---
+++ Python/03 - Strings/[Easy] - String Split and Join.py
@@ -0,0 +1,8 @@
+# Autore:      Christian Luzzetti
+# Mail:        Christian.luzzetti@gmail.com
+# HackerRank:  https://www.hackerrank.com/Cinghiamenisco
+
+def split_and_join(line):
+    tokens = line.split()
+    return "-".join(tokens)
+    
