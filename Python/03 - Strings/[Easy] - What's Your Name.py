382e9f533889882161bea3015ec1953dc7a3c51e
Christian <42942098+Cinghiamenisco@users.noreply.github.com>
2018-10-10 09:39:58 +0200

    Create [Easy] - What's Your Name.py

---
+++ Python/03 - Strings/[Easy] - What's Your Name.py
@@ -0,0 +1,6 @@
+# Autore:      Christian Luzzetti
+# Mail:        Christian.luzzetti@gmail.com
+# HackerRank:  https://www.hackerrank.com/Cinghiamenisco
+
+def print_full_name(a, b):
+    print("Hello " + a + " " + b + "! You just delved into python.")
