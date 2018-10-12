c6401b6530b3344858d3036da065decfb474bbf2
Christian <42942098+Cinghiamenisco@users.noreply.github.com>
2018-10-12 09:28:02 +0200

    Create [Easy] - Set intersection Operation.py

---
+++ Python/04 - Sets/[Easy] - Set intersection Operation.py
@@ -0,0 +1,9 @@
+# Autore:      Christian Luzzetti
+# Mail:        Christian.luzzetti@gmail.com
+# HackerRank:  https://www.hackerrank.com/Cinghiamenisco
+
+card_a = input()
+a = set(map(int, input().split()))
+card_b = input()
+b = set(map(int, input().split()))
+print(len(a.intersection(b)))
