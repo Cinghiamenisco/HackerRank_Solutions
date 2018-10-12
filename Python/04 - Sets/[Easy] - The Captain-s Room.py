04d0a8e2455857501f18464b2ff5dbc836716611
Christian <42942098+Cinghiamenisco@users.noreply.github.com>
2018-10-12 10:40:21 +0200

    Update [Easy] - The Captain-s Room.py

--- Python/04 - Sets/[Easy] - The Captain-s Room.py
+++ Python/04 - Sets/[Easy] - The Captain-s Room.py
@@ -2,8 +2,13 @@
 # Mail:        Christian.luzzetti@gmail.com
 # HackerRank:  https://www.hackerrank.com/Cinghiamenisco
 
-card_a = input()
-numbers = list(map(int,input().split()))
-for n in numbers:
-    if numbers.count(n) == 1:
-        print(n)
+input()
+setA = set()
+setB = set()
+for n in input().split():
+    if n in setA:
+        setB.add(n)
+    else:
+        setA.add(n)
+
+print(setA.difference(setB).pop())
