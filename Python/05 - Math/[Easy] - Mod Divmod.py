ad2c7573291b0ce044f96541b2382b8b84e5fdf3
Christian <42942098+Cinghiamenisco@users.noreply.github.com>
2018-10-12 11:23:05 +0200

    Create [Easy] - Mod Divmod.py

---
+++ Python/05 - Math/[Easy] - Mod Divmod.py
@@ -0,0 +1,9 @@
+# Autore:      Christian Luzzetti
+# Mail:        Christian.luzzetti@gmail.com
+# HackerRank:  https://www.hackerrank.com/Cinghiamenisco
+
+a = int(input())
+b = int(input())
+print(int(a/b))
+print(a%b)
+print(divmod(a,b))
