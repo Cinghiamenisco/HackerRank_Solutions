521c5766d1bd6866f8152d3c0f195ddff3a358f3
Christian <42942098+Cinghiamenisco@users.noreply.github.com>
2018-10-17 11:31:20 +0200

    Update [Medium] - Compress the String.py

--- Python/06 - Itertools/[Medium] - Compress the String.py
+++ Python/06 - Itertools/[Medium] - Compress the String.py
@@ -6,4 +6,9 @@ from itertools import groupby
 
 stringa = input()
 
-print(*[(len(list(g)),int(k)) for k,g in groupby(stringa)])
+b = groupby(stringa)
+for c,d in b:
+    print((len(list(d)),int(c)),end=' ')
+
+#one-Line
+#print(*[(len(list(g)),int(k)) for k,g in groupby(stringa)])
