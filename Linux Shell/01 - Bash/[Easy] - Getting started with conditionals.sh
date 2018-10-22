0b0bb4860a391809d9d0dee80925f55908a233d2
Christian <42942098+luzzetti@users.noreply.github.com>
2018-10-22 14:33:59 +0200

    Create [Easy] - Getting started with conditionals.sh

---
+++ Linux Shell/01 - Bash/[Easy] - Getting started with conditionals.sh
@@ -0,0 +1,10 @@
+# Autore:      Christian Luzzetti
+# Mail:        Christian.luzzetti@gmail.com
+# HackerRank:  https://www.hackerrank.com/Cinghiamenisco
+
+read answer
+if [ "$answer" = "Y" ] || [ "$answer" = "y" ]; then
+    echo "YES"
+elif [ "$answer" = "N" ] || [ "$answer" = "n" ]; then
+    echo "NO"
+fi
