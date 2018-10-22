86d4177f07e75c10f4880b81ca1f65f55fd44f43
Christian <42942098+luzzetti@users.noreply.github.com>
2018-10-22 17:47:46 +0200

    Create [Medium] - Arithmetic Operations.sh

---
+++ Linux Shell/01 - Bash/[Medium] - Arithmetic Operations.sh
@@ -0,0 +1,6 @@
+# Autore:      Christian Luzzetti
+# Mail:        Christian.luzzetti@gmail.com
+# HackerRank:  https://www.hackerrank.com/Cinghiamenisco
+
+read ex
+printf %.3f $( echo $ex | bc -l )
