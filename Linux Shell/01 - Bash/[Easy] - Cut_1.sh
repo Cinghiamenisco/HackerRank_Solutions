cf16b3450dbd045177afa6655228dddad9f09a4a
Christian <42942098+luzzetti@users.noreply.github.com>
2018-10-23 10:00:32 +0200

    Create [Easy] - Cut_1.sh

---
+++ Linux Shell/01 - Bash/[Easy] - Cut_1.sh
@@ -0,0 +1,9 @@
+# Autore:      Christian Luzzetti
+# Mail:        Christian.luzzetti@gmail.com
+# HackerRank:  https://www.hackerrank.com/Cinghiamenisco
+
+DONE=false
+until $DONE ; do
+    read || DONE=true
+    echo $REPLY | cut -c3
+done
