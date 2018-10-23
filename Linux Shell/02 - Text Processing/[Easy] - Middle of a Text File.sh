c9ead224a6333acf90999c4d71374854357b867e
Christian <42942098+luzzetti@users.noreply.github.com>
2018-10-23 12:12:44 +0200

    Create [Easy] - Middle of a Text File.sh

---
+++ Linux Shell/02 - Text Processing/[Easy] - Middle of a Text File.sh
@@ -0,0 +1,6 @@
+# Autore:      Christian Luzzetti
+# Mail:        Christian.luzzetti@gmail.com
+# HackerRank:  https://www.hackerrank.com/Cinghiamenisco
+
+#Display the lines (from line number 12 to 22,
+head -n 22 $1 | tail -n 11 $_
