06a20cf8c496d7a694fc2c73d73e481db30d78a3
Christian <42942098+luzzetti@users.noreply.github.com>
2018-10-23 10:04:32 +0200

    Create [Easy] - Cut_3.sh

---
+++ Linux Shell/02 - Text Processing/[Easy] - Cut_3.sh
@@ -0,0 +1,9 @@
+# Autore:      Christian Luzzetti
+# Mail:        Christian.luzzetti@gmail.com
+# HackerRank:  https://www.hackerrank.com/Cinghiamenisco
+
+DONE=false
+until $DONE ; do
+    read || DONE=true
+    echo $REPLY | cut -c2-7
+done
