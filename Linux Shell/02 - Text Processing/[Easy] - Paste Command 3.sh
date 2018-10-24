f76ba8c710ef1d9018678697d1a98060cad56274
Christian <42942098+luzzetti@users.noreply.github.com>
2018-10-24 10:10:35 +0200

    Update [Easy] - Paste Command 3.sh

--- Linux Shell/02 - Text Processing/[Easy] - Paste Command 3.sh
+++ Linux Shell/02 - Text Processing/[Easy] - Paste Command 3.sh
@@ -2,4 +2,8 @@
 # Mail:        Christian.luzzetti@gmail.com
 # HackerRank:  https://www.hackerrank.com/Cinghiamenisco
 
-paste -s -d ';;\n' - - - $1
+#Option_1
+#tr '\n' '\t'
+
+#Option_2
+paste -sd '\t'
