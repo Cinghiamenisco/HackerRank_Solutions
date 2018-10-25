9cb242f667b3ce23a1f6fc86894402a35beeeea7
Christian <42942098+luzzetti@users.noreply.github.com>
2018-10-25 09:55:04 +0200

    Create [Easy] - Display an element of an array.sh

---
+++ Linux Shell/03 - Arrays in Bash/[Easy] - Display an element of an array.sh
@@ -0,0 +1,12 @@
+# Autore:      Christian Luzzetti
+# Mail:        Christian.luzzetti@gmail.com
+# HackerRank:  https://www.hackerrank.com/Cinghiamenisco
+
+i=0
+for line in `cat $1`;
+do
+    array[$i]=$line
+    let i++
+done
+
+echo ${array[3]}
