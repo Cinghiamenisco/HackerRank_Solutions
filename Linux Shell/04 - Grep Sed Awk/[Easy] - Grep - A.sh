b273b71cd8a12b95aece983b1f8263ac824d5ce9
Christian <42942098+luzzetti@users.noreply.github.com>
2018-10-26 10:03:21 +0200

    Create [Easy] - Grep - A.sh

---
+++ Linux Shell/04 - Grep Sed Awk/[Easy] - Grep - A.sh
@@ -0,0 +1,10 @@
+# Autore:      Christian Luzzetti
+# Mail:        Christian.luzzetti@gmail.com
+# HackerRank:  https://www.hackerrank.com/Cinghiamenisco
+
+#Options:
+#Extended regex
+#ignore Case
+#word - Select only those lines containing matches that form whole words.
+
+grep -Eiw 'th(e|ose|en|at)'
